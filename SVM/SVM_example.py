745fda10997f1d04fcf250a80b9f91eccb62e575
Giuseppe Barbato <giuseppe95barbato@gmail.com>
2021-12-29 21:21:24 +0100

    Added SVM task description

--- SVM/SVM_example.py
+++ SVM/SVM_example.py
@@ -1,11 +1,14 @@
+# We want to classify iris flowers using sepal length, sepal width, petal length and petal width as features.
+# We will create a classification model using SVM algorithm.
+
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
-import pandas
 
 iris_dataset = datasets.load_iris()
 X = iris_dataset.data
 y = iris_dataset.target
 classes = ['setosa', 'versicolor', 'virginica']
+print(iris_dataset)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
